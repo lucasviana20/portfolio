081a6cc740775577fe06778dc17f2ffe2df30702
Lucas Ellery <lucasev2002@gmail.com>
2025-02-25 17:55:16 -0300

    first-component

--- portfolio-site/src/app/app.component.ts
+++ portfolio-site/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FirstComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ portfolio-site/src/app/components/first-component/first-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first-component', //nome da tag do componente
+  imports: [],
+  templateUrl: './first-component.component.html',
+  styleUrl: './first-component.component.css' //style exclusivo do componente
+})
+export class FirstComponentComponent {
+
+}
