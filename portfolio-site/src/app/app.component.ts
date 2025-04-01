8fda8114557927248f9831343a3ce55810fadf7e
Lucas Ellery <lucasev2002@gmail.com>
2025-03-31 21:43:24 -0300

    languages

--- portfolio-site/src/app/app.component.ts
+++ portfolio-site/src/app/app.component.ts
@@ -4,10 +4,11 @@ import { FirstComponentComponent } from './components/first-component/first-comp
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { EducationComponent } from './components/education/education.component';
+import { LanguagesComponent } from './components/languages/languages.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FirstComponentComponent, NavbarComponent, HomeComponent,EducationComponent],
+  imports: [RouterOutlet, FirstComponentComponent, NavbarComponent, HomeComponent,EducationComponent, LanguagesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ portfolio-site/src/app/components/languages/languages.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-languages',
+  imports: [],
+  templateUrl: './languages.component.html',
+  styleUrl: './languages.component.css'
+})
+export class LanguagesComponent {
+
+}
